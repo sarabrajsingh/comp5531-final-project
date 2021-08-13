0f365d84f350f815f67d8a5230c9f938e13e5451
tomlamouret <tom.lamouret@gmail.com>
2021-08-13 11:53:51 -0400

    Caught a typo

--- auth/verify-login.php
+++ auth/verify-login.php
@@ -16,7 +16,7 @@ if ( !isset($_POST['login-email'], $_POST['login-password']) ) {
 
 // Prepare our SQL, preparing the SQL statement will prevent SQL injection.
 
-if ($$stmt = $con->prepare('SELECT password FROM users WHERE email = ? UNION SELECT password FROM companies WHERE email = ?;')) {
+if ($stmt = $con->prepare('SELECT password FROM users WHERE email = ? UNION SELECT password FROM companies WHERE email = ?;')) {
 	// Bind parameters (s = string, i = int, b = blob, etc), in our case the username is a string so we use "s"
 
 	$stmt->bind_param('ss', $_POST['login-email'], $_POST['login-email']);
