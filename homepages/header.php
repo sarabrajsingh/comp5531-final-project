468b151208948ea39225a2e072f5828d42b1220a
tomlamouret <tom.lamouret@gmail.com>
2021-08-13 13:02:23 -0400

    Trying something

--- homepages/header.php
+++ homepages/header.php
@@ -10,6 +10,16 @@ if (isset($_SESSION['employerStatus'])){
 
 <!DOCTYPE html>
 <html>
+<head>
+		<meta charset="utf-8">
+		<title>Home Page</title>
+		<link href="css/style.css" rel="stylesheet" type="text/css">
+		<link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.7.1/css/all.css">
+		<link href="//maxcdn.bootstrapcdn.com/bootstrap/4.1.1/css/bootstrap.min.css" rel="stylesheet" id="bootstrap-css">
+		<script src="//maxcdn.bootstrapcdn.com/bootstrap/4.1.1/js/bootstrap.min.js"></script>
+		<script src="//cdnjs.cloudflare.com/ajax/libs/jquery/3.2.1/jquery.min.js"></script>
+</head>
+
 	<nav class="navtop">
 		<div>
 			<h1><a href=<?= $redirect_path?>>Job Findr</a></h1>
