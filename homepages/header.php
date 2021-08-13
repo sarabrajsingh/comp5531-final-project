5060f5229fde29674b6f1306a7abb658ce33c013
tomlamouret <tom.lamouret@gmail.com>
2021-08-13 14:37:33 -0400

    Trying a different style

--- homepages/header.php
+++ homepages/header.php
@@ -11,11 +11,23 @@ if (isset($_SESSION['employerStatus'])){
 <!DOCTYPE html>
 <html>
 
-	<nav class="navtop">
+	<!-- <nav class="navtop">
 		<div>
 			<h1 style="display:inline"><a href=<?=$redirect_path?>>Job Findr</a> </h1>
 			<a href="profile.php"><i class="fas fa-user-circle"></i>Profile</a>
 			<a href="../logout.php"><i class="fas fa-sign-out-alt"></i>Logout</a>
 		</div>
-	</nav>
+	</nav> -->
+   
+    <nav class="navbar navbar-default">
+  <div class="container-fluid">
+    <div class="navbar-header">
+      <a class="navbar-brand" href=<?=$redirect_path?>>Job Findr</a> 
+    </div>
+    <ul class="nav navbar-nav navbar-right">
+      <li><a href="profile.php"><i class="fas fa-user-circle"></i>Profile</a></li>
+      <li><a href="../logout.php"><i class="fas fa-sign-out-alt"></i>Logout</a></li>
+    </ul>
+  </div>
+</nav>
 </html>
