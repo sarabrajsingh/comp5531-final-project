5987dcced33c57307246641e74d502dd944f7fa1
tomlamouret <tom.lamouret@gmail.com>
2021-08-14 16:37:45 -0400

    Profile test

---
+++ homepages/load-attributes.php
@@ -0,0 +1,13 @@
+<?php
+// This script loads the remaining attributes of the users table into $_SESSION
+session_start();
+
+if ($stmt = $con->prepare("SELECT name, dob, subscriptionLevel, paymentInfos, isActive FROM users WHERE email = ?")){
+    $stmt->bind_param('ssssb', $_SESSION['email']);
+	$stmt->execute();
+    $stmt->bind_result($_SESSION['name'], $_SESSION['dob'], $_SESSION['subscriptionLevel'], $_SESSION['paymentInfos'], $_SESSION['isActive']);
+    $stmt->fetch();
+    $stmt->close();
+} else {
+    exit('Invalid SQL query.');
+}
---
+++ homepages/profile/profile.php
@@ -0,0 +1,60 @@
+<?php
+// We need to use sessions, so you should always start sessions using the below code.
+session_start();
+require '../database/db.php';
+// If the user is not logged in redirect to the login page...
+if (!isset($_SESSION['loggedin'])) {
+	header('Location: ../index.html');
+	exit;
+}
+?>
+
+
+<!DOCTYPE html>
+<html>
+	<head>
+		<meta charset="utf-8">
+		<title>Profile Page</title>
+		<link href="css/style.css" rel="stylesheet" type="text/css">
+		<link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.7.1/css/all.css">
+		<link href="//maxcdn.bootstrapcdn.com/bootstrap/4.1.1/css/bootstrap.min.css" rel="stylesheet" id="bootstrap-css">
+		<script src="//maxcdn.bootstrapcdn.com/bootstrap/4.1.1/js/bootstrap.min.js"></script>
+		<script src="//cdnjs.cloudflare.com/ajax/libs/jquery/3.2.1/jquery.min.js"></script>
+	</head>
+
+	<body class="loggedin">
+		<?php require 'header.php'; ?>
+
+<link href="https://maxcdn.bootstrapcdn.com/font-awesome/4.6.3/css/font-awesome.min.css" rel="stylesheet" integrity="#" crossorigin="anonymous">
+
+<div class="content">
+			<h2>Profile Page</h2>
+			<div>
+				<p>Your account details are below:</p>
+				<table>
+					<tr>
+						<td> Name:</td>
+						<td><?=$_SESSION['name'];?></td>
+					</tr>
+					<tr>
+						<td> Date of birth:</td>
+						<td><?=$_SESSION['dob'];?></td>
+					</tr>
+					<tr>
+						<td>Email:</td>
+						<td><?=$_SESSION['email'];?></td>
+					</tr>
+					<tr>
+						<td>Password:</td>
+						<td><?=$_SESSION['password'];?></td>
+					</tr>
+					<tr>
+						<td> Subscription Level:</td>
+						<td><?=$_SESSION['subscriptionLevel'];?></td>
+					</tr>
+				</table>
+				<button onclick="window.location='edit-profile.php'">Edit</button>
+			</div>
+		</div>
+	</body>
+</html>
