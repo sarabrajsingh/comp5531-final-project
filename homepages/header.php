d85add71629c7f169ff995c191a754f7a30f9f88
tomlamouret <tom.lamouret@gmail.com>
2021-08-14 16:44:44 -0400

    Wrong path

--- homepages/header.php
+++ homepages/header.php
@@ -25,7 +25,7 @@ if (isset($_SESSION['employerStatus'])){
       <a class="navbar-brand" href=<?=$redirect_path?>>Job Findr</a> 
     </div>
     <ul class="nav navbar-nav navbar-right">
-      <li><a href="profile.php"><i class="fas fa-user-circle"></i>Profile</a></li>
+      <li><a href="../profile/profile.php"><i class="fas fa-user-circle"></i>Profile</a></li>
       <li><a href="../logout.php"><i class="fas fa-sign-out-alt"></i>Logout</a></li>
     </ul>
   </div>
