9fe563260f96bc87c096a672b5b7001745f3f545
tomlamouret <tom.lamouret@gmail.com>
2021-08-14 23:36:36 -0400

    Test

--- profile/profile.php
+++ profile/profile.php
@@ -51,32 +51,33 @@ if (!isset($_SESSION['loggedin'])) {
 					<td> <label for="password">Password: </label></td>
 					<td><input type='password' name='password' id='password' value='<?= $_SESSION['password']; ?>' readonly></td>
 				</tr>
-				<tr>
-					<td>Subscription Level: </td>
-					<td>
-						<?php if ($_SESSION['type'] === 'user') : ?>
-							<?php if ($_SESSION['subscriptionLevel'] === 'Basic') : ?>
-								<label><input type="radio" name="subscriptionLevel" value="basic" checked readonly> Basic - Free</label><br>
-							<?php else : ?>
-								<label><input type="radio" name="subscriptionLevel" value="basic" readonly> Basic - Free</label><br>
+				<?php if ($_SESSION['type'] != 'admin') : ?>
+					<tr>
+						<td>Subscription Level: </td>
+						<td>
+							<?php if ($_SESSION['type'] === 'user') : ?>
+								<?php if ($_SESSION['subscriptionLevel'] === 'Basic') : ?>
+									<label><input type="radio" name="subscriptionLevel" value="basic" checked readonly> Basic - Free</label><br>
+								<?php else : ?>
+									<label><input type="radio" name="subscriptionLevel" value="basic" readonly> Basic - Free</label><br>
+								<?php endif; ?>
 							<?php endif; ?>
-						<?php endif; ?>
-						<?php if ($_SESSION['subscriptionLevel'] === 'Gold') : ?>
-							<label><input type="radio" name="subscriptionLevel" value="gold" checked readonly> Gold - $10/month</label><br>
-						<?php else : ?>
-							<label><input type="radio" name="subscriptionLevel" value="gold" readonly> Gold - $10/month</label><br>
-						<?php endif; ?>
-						<?php if ($_SESSION['subscriptionLevel'] === 'Prime') : ?>
-							<label><input type="radio" name="subscriptionLevel" value="prime" checked readonly> Prime - $20/month</label>
-						<?php else : ?>
-							<label><input type="radio" name="subscriptionLevel" value="prime" readonly> Prime - $20/month</label>
-						<?php endif; ?>
-					</td>
-				</tr>
+							<?php if ($_SESSION['subscriptionLevel'] === 'Gold') : ?>
+								<label><input type="radio" name="subscriptionLevel" value="gold" checked readonly> Gold - $10/month</label><br>
+							<?php else : ?>
+								<label><input type="radio" name="subscriptionLevel" value="gold" readonly> Gold - $10/month</label><br>
+							<?php endif; ?>
+							<?php if ($_SESSION['subscriptionLevel'] === 'Prime') : ?>
+								<label><input type="radio" name="subscriptionLevel" value="prime" checked readonly> Prime - $20/month</label>
+							<?php else : ?>
+								<label><input type="radio" name="subscriptionLevel" value="prime" readonly> Prime - $20/month</label>
+							<?php endif; ?>
+						</td>
+					</tr>
+				<?php endif; ?>
 			</table>
 			<button onclick="window.location='edit-profile.php'">Edit profile</button>
 			<button onclick="window.location='delete-profile.php'">Delete profile</button>
-		<?php endif; ?>
 		</div>
 	</div>
 </body>
