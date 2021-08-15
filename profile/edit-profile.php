4d37b0a4fe9f8a6ca07e00f09531c6b25f834a57
tomlamouret <tom.lamouret@gmail.com>
2021-08-14 23:18:07 -0400

    Testing

--- profile/edit-profile.php
+++ profile/edit-profile.php
@@ -25,32 +25,43 @@ session_start();
 		<fieldset>
 			<table>
 				<tr>
-					<td> <label for="name">Name </label>:</td>
+					<td> <label for="name">Name : </label></td>
 					<td><input type='text' name='name' id='name' value=<?= $_SESSION['name']; ?>></td>
 				</tr>
 				<?php if ($_SESSION['type'] === 'user') : ?>
 					<tr>
-						<td> <label for="dob">Date of Birth </label>:</td>
+						<td> <label for="dob">Date of Birth : </label></td>
 						<td><input type='date' name='dob' id='dob' value=<?= $_SESSION['dob']; ?>></td>
 					</tr>
 				<?php endif; ?>
 				<tr>
-					<td> <label for="email">Email </label>:</td>
+					<td> <label for="email">Email : </label></td>
 					<td><input type='email' name='email' id='email' value=<?= $_SESSION['email']; ?> readonly></td>
 				</tr>
 				<tr>
-					<td> <label for="password">Password </label>:</td>
+					<td> <label for="password">Password : </label></td>
 					<td><input type='password' name='password' id='password' value=<?= $_SESSION['password']; ?>></td>
 				</tr>
 				<tr>
-					<td> <label for="subscriptionLevel">Subscription Level </label>:</td>
+					<td> <legend>Subscription Level: </legend></td>
 					<td>
 						<?php if ($_SESSION['type'] === 'user') : ?>
-							<label><input type="radio" name="subscriptionLevel" value="basic">Basic - Free</label><br>
+							<?php if ($_SESSION['subscriptionLevel'] === 'Basic') : ?>
+								<label><input type="radio" name="subscriptionLevel" value="basic" checked> Basic - Free</label><br>
+							<?php else : ?>
+								<label><input type="radio" name="subscriptionLevel" value="basic"> Basic - Free</label><br>
+							<?php endif; ?>
 						<?php endif; ?>
-						<label><input type="radio" name="subscriptionLevel" value="gold">Gold - $10/month</label><br>
-						<label><input type="radio" name="subscriptionLevel" value="prime">Prime - $20/month</label>
-						</select>
+						<?php if ($_SESSION['subscriptionLevel'] === 'Gold') : ?>
+							<label><input type="radio" name="subscriptionLevel" value="gold" checked> Gold - $10/month</label><br>
+						<?php else : ?>
+							<label><input type="radio" name="subscriptionLevel" value="gold"> Gold - $10/month</label><br>
+						<?php endif; ?>
+						<?php if ($_SESSION['subscriptionLevel'] === 'Prime') : ?>
+							<label><input type="radio" name="subscriptionLevel" value="prime" checked> Prime - $20/month</label>
+						<?php else : ?>
+							<label><input type="radio" name="subscriptionLevel" value="prime"> Prime - $20/month</label>
+						<?php endif; ?>
 					</td>
 				</tr>
 			</table>
